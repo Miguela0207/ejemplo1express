b57b7c77d26aa74e1deeed4c7669f5c1a64e5567
Miguel <miguelap3104@gmail.com>
2024-04-15 16:18:28 -0500

    Token
    Generamos métodos de protección de API

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require("./validate_token");
+
 //Nuevo animal
-router.post("/animals", (req, res) => {
+router.post("/animals", verifyToken, (req, res) => {
     const animal = animalSchema(req.body);
     animal
         .save()
@@ -10,3 +12,51 @@ router.post("/animals", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 module.exports = router;
+
+//Consultar todos los animales
+router.get("/animals", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+//Consultar un animal por su id
+router.get("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+//Modificar el nombre de un animal por su id
+router.put("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+//Eliminar un animal por su id
+
+router.delete("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
+
+
